c3a180502d1c9b6d4e5f08be5b4cb5bc4ca28878
Prajwal264 <prajwalp264@gmail.com>
2023-06-07 13:44:47 +0530

    feat: password validations in login page

--- apps/web/src/components/form-field/form-field.component.tsx
+++ apps/web/src/components/form-field/form-field.component.tsx
@@ -3,6 +3,7 @@
 import React, { FC, HTMLInputTypeAttribute, useState, useMemo } from 'react';
 import styles from './form-field.module.scss';
 import PasswordIcon from '../../assets/password-icon.png';
+import { Tooltip } from 'react-tooltip';
 
 interface Props {
   type?: HTMLInputTypeAttribute;
@@ -10,9 +11,13 @@ interface Props {
   label: string;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   inputAttributes?: React.InputHTMLAttributes<HTMLInputElement>;
+  className?: string;
+  errorAttr?: {
+    errorValue: string;
+  }
 }
 
-const FormInput: FC<Props> = ({ type = 'text', name, label, inputAttributes = {}, onChange }) => {
+const FormInput: FC<Props> = ({ type = 'text', name, label, inputAttributes = {}, onChange, errorAttr }) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -32,8 +37,13 @@ const FormInput: FC<Props> = ({ type = 'text', name, label, inputAttributes = {}
   return (
     <div className={styles.formField}>
       <label htmlFor={name}>{label}</label>
-      <input type={inputType} name={name} id={name} onChange={handleChange} {...inputAttributes} />
+      <input
+        data-tooltip-id="error-tooltip"
+        data-tooltip-content={errorAttr?.errorValue}
+        data-tooltip-place="top"
+        type={inputType} name={name} id={name} onChange={handleChange} {...inputAttributes} />
       {type === 'password' && <img onClick={() => setShowPassword(!showPassword)} src={PasswordIcon} alt="password" />}
+      <Tooltip isOpen={!!errorAttr?.errorValue} id="error-tooltip" place='top'></Tooltip>
     </div>
   );
 };
--- apps/web/src/pages/login/login.page.tsx
+++ apps/web/src/pages/login/login.page.tsx
@@ -6,6 +6,8 @@ import FormInput from '../../components/form-field/form-field.component';
 import { useCallback, useState } from 'react';
 import User, { LoginPayload } from '../../services/api/user.api.service';
 import { setAccessToken, setRefreshToken } from '../../helpers/token.helper';
+import { validateEmail } from '../../helpers/validation.helpers';
+import { Tooltip } from 'react-tooltip';
 
 interface ILoginPageProps { }
 
@@ -14,11 +16,16 @@ const LoginPage: React.FC<ILoginPageProps> = () => {
     email: '',
     password: '',
   });
+  const [errorMap, setErrorMap] = useState({
+    email: '',
+    password: ''
+  })
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
     (e) => {
       const { name, value } = e.target;
+      validateErrors(name, value);
       setFormData({
         ...formData,
         [name]: value,
@@ -26,6 +33,35 @@ const LoginPage: React.FC<ILoginPageProps> = () => {
     },
     [formData],
   );
+
+  const validateErrors = (field: string, value: string) => {
+    if (field === 'email') {
+      if (!validateEmail(value)) {
+        setErrorMap({
+          ...errorMap,
+          email: 'Please Enter a Valid Email',
+        })
+      } else {
+        setErrorMap({
+          ...errorMap,
+          email: '',
+        })
+      }
+    } else if (field === 'password') {
+      const re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+      if (!re.test(value)) {
+        setErrorMap({
+          ...errorMap,
+          password: 'Please enter an 8 letter password, with at least a symbol, upper and lower case letters and a number',
+        })
+      } else {
+        setErrorMap({
+          ...errorMap,
+          password: '',
+        })
+      }
+    }
+  }
   const login: React.MouseEventHandler<HTMLButtonElement> = useCallback(
     async (e) => {
       e.stopPropagation();
@@ -52,11 +88,23 @@ const LoginPage: React.FC<ILoginPageProps> = () => {
           <img src={logo} alt="logo" width={170} />
         </NavLink>
         <h1 className={styles.loginFormHeader}>Log in</h1>
-        <FormInput type="email" name="email" label={'Email'} onChange={handleChange} />
-        <FormInput type="password" name="password" label={'Password'} onChange={handleChange} />
+        <FormInput type="email" name="email" label={'Email'} onChange={handleChange} inputAttributes={{
+          className: errorMap.email ? 'error' : '',
+        }}
+          errorAttr={{
+            errorValue: errorMap.email
+          }}
+        />
+        <FormInput type="password" name="password" label={'Password'} onChange={handleChange}
+          inputAttributes={{
+            className: errorMap.password ? 'error' : '',
+          }}
+          errorAttr={{
+            errorValue: errorMap.password
+          }} />
         <PrimaryButton content="Log in" type="button" onClick={login} loading={loading} />
         <div className={styles.note}>
-          By continuing with Google, Apple, or Email, you agree to DoneDone Terms of Service and Privacy Policy .
+          By continuing you agree to DoneDone Terms of Service and Privacy Policy .
         </div>
         <div className={styles.seperator}></div>
         <div className={styles.moveToSignCta}>
