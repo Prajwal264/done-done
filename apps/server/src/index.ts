283374a3dbf2c8fe3f94020bda6b9a2281eeb94e
Prajwal264 <prajwalp264@gmail.com>
2023-06-07 20:15:45 +0530

    update client origin

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -87,7 +87,7 @@ class Server {
     app.use(express.json());
     app.use(cors({
       credentials: true,
-      origin: process.env.CLIENT_ORIGIN,
+      origin: process.env.CLIENT_ORIGIN || 'https://done-done.onrender.com',
     }));
     const server = new InversifyExpressServer(container, null, { rootPath: '/api' }, app);
     this.app = server.build();
