92809844320c0c6aee60ebef183bc8cc9ed8f67e
Prajwal264 <prajwalp264@gmail.com>
2023-06-08 00:40:16 +0530

    fix: UI review fixes

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './app.component';
-import 'react-tooltip/dist/react-tooltip.css'
+import 'react-tooltip/dist/react-tooltip.css';
 import './styles/index.scss';
 import { Toaster } from 'react-hot-toast';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-    <Toaster position="bottom-right" />
+    <Toaster position="top-right" />
   </BrowserRouter>,
   document.getElementById('root'),
 );
